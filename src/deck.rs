64b120a14d342dfa8492a818669af809c7d048c6
Mårten Nilsson <m@martennilsson.se>
2020-03-22 22:32:46 +0100

    Deck generation from iterators done

--- src/deck.rs
+++ src/deck.rs
@@ -1,5 +1,24 @@
+use std::iter::IntoIterator;
+
+use rand::seq::SliceRandom;
+
 use crate::card;
+
 
 pub struct Deck<CardType: card::Card> {
     cards: Vec<CardType>,
+}
+
+impl<CardType: card::Card> Deck<CardType> {
+    pub fn shuffle(&mut self){
+        let mut rng = rand::thread_rng();
+        self.cards.shuffle(&mut rng);
+    }
+}
+
+
+impl <CardType: card::Card> std::iter::FromIterator<u8> for Deck<CardType>{
+    fn from_iter<I: IntoIterator<Item=u8>>(iter: I) -> Self {
+        Deck{cards: iter.into_iter().map(CardType::from).collect()}
+    }
 }
