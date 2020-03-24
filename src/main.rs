4b7542c5f0938fbe5d49bf521e0cf8b309e68c2c
Mårten Nilsson <m@martennilsson.se>
2020-03-24 20:50:55 +0100

    Deck creation endpoint implemented

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+use std::fs::File;
+
+use rocket_contrib::json::Json;
+
 use plumpinator::card::PlayingCard;
 use plumpinator::deck::Deck;
 
+
+#[get("/deck/<name>")]
+fn create_deck(name: String) -> Json<Deck<PlayingCard>> {
+    let mut deck: Deck<PlayingCard> = Deck::new52();
+    deck.shuffle();
+
+    let mut file = File::create(format!("{}.json", name)).unwrap();
+    serde_json::to_writer_pretty(file, &deck).unwrap();
+
+    Json(deck)
+}
+
+
 fn main() {
     let mut deck: Deck<PlayingCard> = Deck::new52();
-    println!("Hello, world!");
-    println!("{:?}", deck);
 
     deck.shuffle();
     let my_hand = deck.take(8);
-    let your_hand = deck.take(8);
-
-    println!(".....");
-    println!("My hand: {:?}", my_hand);
-    println!("Your hand: {:?}", your_hand);
 
     println!("{}", serde_json::to_string_pretty(&my_hand).unwrap());
+
+    let mut file = File::create("hello.json").unwrap();
+    serde_json::to_writer_pretty(file, &my_hand).unwrap();
+
+    rocket::ignite().mount("/", routes![create_deck]).launch();
 }
