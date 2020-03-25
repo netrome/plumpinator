647e8b5edc8224a065618d45e653d9ce336356ed
Mårten Nilsson <m@martennilsson.se>
2020-03-25 18:46:56 +0100

    Getting a hand from a deck implemented

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,30 @@ fn create_deck(name: String) -> Json<Deck<PlayingCard>> {
     let mut deck: Deck<PlayingCard> = Deck::new52();
     deck.shuffle();
 
-    let mut file = File::create(format!("{}.json", name)).unwrap();
+    let file = File::create(format!("{}.json", name)).unwrap();
     serde_json::to_writer_pretty(file, &deck).unwrap();
 
     Json(deck)
+}
+
+
+#[get("/deck/<name>/<number>")]
+fn get_hand(name: String, number: usize) -> Json<Deck<PlayingCard>> {
+    let mut deck = if let Ok(file) = File::open(format!("{}.json", name))
+    {
+        serde_json::from_reader(file).unwrap()
+    }
+    else
+    {
+        Deck::new52()
+    };
+
+    let hand = deck.take(number);
+
+    let file = File::create(format!("{}.json", name)).unwrap();
+    serde_json::to_writer_pretty(file, &deck).unwrap();
+
+    Json(hand)
 }
 
 
@@ -30,8 +50,8 @@ fn main() {
 
     println!("{}", serde_json::to_string_pretty(&my_hand).unwrap());
 
-    let mut file = File::create("hello.json").unwrap();
+    let file = File::create("hello.json").unwrap();
     serde_json::to_writer_pretty(file, &my_hand).unwrap();
 
-    rocket::ignite().mount("/", routes![create_deck]).launch();
+    rocket::ignite().mount("/", routes![create_deck, get_hand]).launch();
 }
